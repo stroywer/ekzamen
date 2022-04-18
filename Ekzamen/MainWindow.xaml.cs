da2a9642927bfdcb6dc947bec0aa74331c9b90ea
unknown <@414-10.KPC.RU>
2022-04-18 09:45:47 +0300

    first commit

---
+++ Ekzamen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ekzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Vxod_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new VxodPage());
+        }
+
+        private void Now_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new NowPage());
+        }
+
+        private void NowFuture_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new NewFuturePage());
+        }
+    }
+}
---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a8c3de2-3951-4a4b-b0f7-0059965bef28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
